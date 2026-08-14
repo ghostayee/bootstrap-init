80d67931b991135cc42bac3b9b6083c56b30e922
ghostayee <mayeeayee@outlook.com>
2026-08-14 14:52:44 +0300

    ui:

---
+++ app.py
@@ -0,0 +1,15 @@
+import psycopg2
+
+from flask import Flask, render_template, redirect, request, urf_for
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ in "__main__":
+    app.run(debug=True)
